fc93321ed4f859f71ab9d74e17a000d61ad4161c
Sophie <primesophie.2020@gmail.com>
2020-10-15 12:27:01 -0500

    registerForm

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,70 +1,102 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {HashRouter, Link} from 'react-router-dom';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+
+
+//materialUI & styling
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
+
 class RegisterForm extends Component {
-  state = {
-    username: '',
-    password: '',
-  };
+            state = {
+                  username: '',
+                  password: '',
+                  helperTextUsername: '',
+                  helperTextPassword: '',
+                  error: false
+            }; //end state
 
-  registerUser = (event) => {
-    event.preventDefault();
+            registerUser = (event) => {
+                      event.preventDefault();
 
-    this.props.dispatch({
-      type: 'REGISTER',
-      payload: {
-        username: this.state.username,
-        password: this.state.password,
-      },
-    });
-  }; // end registerUser
+                      if (this.state.username.length>5 && this.state.password.length>10) {
+                            this.props.dispatch({
+                              type: 'REGISTER',
+                              payload: {
+                                username: this.state.username,
+                                password: this.state.password,
+                              },
+                            });
+                      } else {
+                            this.setState({
+                              helperTextUsername: 'Username must be 5 characters or more',
+                              helperTextPassword: 'Password must be 10 characters or more',
+                              error: true
+                            })
+                            this.props.dispatch({ type: 'REGISTRATION_INPUT_ERROR' });
+                      }
+            }; // end registerUser
 
-  handleInputChangeFor = (propertyName) => (event) => {
-    this.setState({
-      [propertyName]: event.target.value,
-    });
-  };
+            handleInputChangeFor = (propertyName) => (event) => {
+              this.setState({
+                [propertyName]: event.target.value,
+              });
+            }; //end handleInputChange
 
   render() {
     return (
-      <form className="formPanel" onSubmit={this.registerUser}>
-        <h2>Register User</h2>
-        {this.props.store.errors.registrationMessage && (
-          <h3 className="alert" role="alert">
-            {this.props.store.errors.registrationMessage}
-          </h3>
-        )}
-        <div>
-          <label htmlFor="username">
-            Username:
-            <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              required
-              onChange={this.handleInputChangeFor('username')}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="password">
-            Password:
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              required
-              onChange={this.handleInputChangeFor('password')}
-            />
-          </label>
-        </div>
-        <div>
-          <input className="btn" type="submit" name="submit" value="Register" />
-        </div>
-      </form>
+      <HashRouter>
+            <div className="login-div">
+            <form>
+            <h2 id="login-title">Login</h2>
+                  <div className="username-login">
+                    <TextField 
+                            error={this.state.error}
+                            helperText={this.state.helperTextUsername}
+                            name="username"
+                            required
+                            fullWidth
+                            variant="outlined"
+                            value={this.state.username}
+                            label="Username"
+                            onChange={this.handleInputChangeFor('username')}
+                          />
+                  </div>
+                  <div className="password-login">
+                    <TextField
+                            error={this.state.error}
+                            helperText={this.state.helperTextPassword}
+                            name="password"
+                            required
+                            fullWidth
+                            variant="outlined"
+                            value={this.state.password}
+                            label="Password"
+                            onChange={this.handleInputChangeFor('password')}
+                          />
+                  </div>
+                <div id="register-button">
+                  <Button 
+                        id="register-btn"
+                        endIcon ={<ArrowForwardIcon/>}
+                        onClick={this.registerUser}>
+                        Login
+                  </Button>
+                </div>
+              </form>
+            </div>    
+      </HashRouter>
     );
   }
-}
+} //end render
 
 export default connect(mapStoreToProps)(RegisterForm);
+
+
+//sources: https://stackoverflow.com/questions/49421792/how-to-use-material-uinext-textfield-error-props
+//sources: https://stackoverflow.com/questions/35901440/how-to-invalidate-a-textfield-in-material-ui
